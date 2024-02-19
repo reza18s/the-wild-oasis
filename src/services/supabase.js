168dedf10d06798ee7a42798c916252ef389f1c2
reza 16t <swary2021@gmail.com>
2024-02-19 23:03:27 +0330

    built the project

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dzbtovlpqauiybttpfod.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6YnRvdmxwcWF1aXlidHRwZm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDExOTA2NTcsImV4cCI6MjAxNjc2NjY1N30.RgzCty4bjDhM9iTZi-LFBMUTsDIaBgOKy0bLW-C-M7g";
+export const supabaseUrl = import.meta.env.VITE_S_URL;
+const supabaseKey = import.meta.env.VITE_S_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
